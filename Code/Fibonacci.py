d3e61c3b16331058f4bd1b84dc8b8ff1b6df2822
KelvinYu <ykq1312@126.com>
2019-09-10 17:01:45 -0400

    Update Fibonacci.py

--- Code/Fibonacci.py
+++ Code/Fibonacci.py
@@ -8,7 +8,7 @@ class Solution:
         elif n == 1:
             return 1
         elif n > 1:
-            return self.Fibonacci(n - 2) + self.Fibonacci(n-1)
+            return self.Fibonacci1(n - 2) + self.Fibonacci1(n-1)
         
     def Fibonacci2(self, n):
         # 循环的方式
@@ -18,6 +18,7 @@ class Solution:
             return 1
         a = 1
         b = 0
+        ret = 0
         
         for i in range(0, n - 1):
             ret = a + b
@@ -28,5 +29,5 @@ class Solution:
           
 if __name__ == '__main__':
     s = Solution()
-    n = input()
-    s.Fibonacci(n)
+    s.Fibonacci1(n) # n需要替换成任意一个数字
+    s.Fibonacci2(n) # n需要替换成任意一个数字
